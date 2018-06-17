0a0c0f1bd9ee9965bc3fa228cbdaf986267b0024
Nagpal <anand.nagpal@accenture.com>
2018-06-17 22:20:59 +0530

    Modified to maven project

--- src/main/java/mylib/springmvc/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package mylib.springmvc.dao;
-
-import mylib.springmvc.model.Login;
-import mylib.springmvc.model.User;
-
-public interface UserDao {
-  void register(User user);
-  User validateUser(Login login);
-}
